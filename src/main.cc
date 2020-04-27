c6748bccbc45cc3a88329ba48fbb006c0cf27c77
bcaluneo <caluneo.brendan@gmail.com>
2020-04-26 23:34:16 -0400

    Added file menu.

--- src/main.cc
+++ src/main.cc
@@ -2,25 +2,53 @@
 
 #define _WIN32_WINNT 0x0501
 #include <Windows.h>
+#include <iostream>
 #include "SDL.h"
 #include "SDL_thread.h"
 #include "SDL_image.h"
+#include "SDL_syswm.h"
 #include "data.hh"
 #include "graph.hh"
+#include "resource.hh"
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 240;
-const int BAR_X = 15;
-const int BAR_Y = 40;
-const int BAR_SCALE = 4;
-const int BAR_WIDTH = SCREEN_WIDTH - (BAR_X*2);
-const int BAR_HEIGHT = 50;
-const int POLL_TIME = 200;
-const int BAND_WIDTH = 5;
+const size_t MENU_HEIGHT = 20;
+const size_t SCREEN_WIDTH = 640;
+const size_t SCREEN_HEIGHT = 260;
+const size_t BAR_X = 15;
+const size_t BAR_Y = 40;
+const size_t BAR_SCALE = 4;
+const size_t BAR_WIDTH = SCREEN_WIDTH - (BAR_X*2);
+const size_t BAR_HEIGHT = 50;
+const size_t POLL_TIME = 200;
+const size_t BAND_WIDTH = 5;
+const size_t TOTAL_BANDS = BAR_WIDTH / BAND_WIDTH;
 
 bool quit = 0;
 SDL_Window *window;
 SDL_Renderer *render;
+
+HWND getHWND() {
+   SDL_SysWMinfo wndInfo;
+	 SDL_VERSION(&wndInfo.version);
+   SDL_GetWindowWMInfo(window, &wndInfo);
+   return wndInfo.info.win.window;
+}
+
+void WndProc(void *userData, void *hwnd, unsigned int message,
+	long long unsigned int wParam, long long int lParam)
+{
+    switch (message)
+    {
+			case WM_COMMAND:
+				switch (LOWORD(wParam)) {
+					case ID_EXIT:
+						quit = 1;
+						break;
+				}
+
+				break;
+		}
+}
 
 int main(int argc, char **args) {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -28,8 +56,12 @@ int main(int argc, char **args) {
 
 	window = SDL_CreateWindow("osview", SDL_WINDOWPOS_CENTERED,
 														SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
-														SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+														SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_ALWAYS_ON_TOP);
 	render = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+	SetMenu(getHWND(), LoadMenu(NULL, MAKEINTRESOURCE(100)));
+
+	SDL_SetWindowsMessageHook(WndProc, NULL);
 
 	SDL_Surface *icon = IMG_Load("tex/icon.png");
 	SDL_SetWindowIcon(window, icon);
@@ -62,7 +94,7 @@ int main(int argc, char **args) {
 	Graph memGraph(2, 1);
 	memGraph.setColors(memColors);
 
-	Graph graphs[2] = {cpuGraph, memGraph};
+	Graph graphs[2] = { cpuGraph, memGraph };
 
 	SDL_CreateThread(getData, "Data Thread", static_cast<void*>(graphs));
 
@@ -92,7 +124,7 @@ int main(int argc, char **args) {
 		bg.x = 0;
 		bg.y = 0;
 		bg.w = SCREEN_WIDTH;
-		bg.h = SCREEN_HEIGHT;
+		bg.h = SCREEN_HEIGHT - MENU_HEIGHT;
 
 		bar.x = BAR_X-1;
 		bar.y = BAR_Y-1;
@@ -112,5 +144,6 @@ int main(int argc, char **args) {
 	}
 
 	SDL_Quit();
+	std::cout << std::endl;
 	return 0;
 }
